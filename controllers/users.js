fd00578b04e72724f09fc4fb149a0a0ebeef2d57
workwithom <om7489874168@gmail.com>
2025-06-23 02:58:28 +0530

    hi

--- controllers/users.js
+++ controllers/users.js
@@ -2,16 +2,17 @@ const User = require("../models/user"); // Import the User model
 const passport = require("passport"); // Import passport for authentication
 const { savereturnTo } = require("../middleware");
 module.exports.renderSignupForm =  (req, res) => {
-    res.render("users/signup.ejs") // Access error message from flash   
+    res.render("users/signup.ejs") // Access error message from flash
 }
 module.exports.renderLoginForm =(req, res) => {
-    res.render("users/login.ejs") // Access error message from flash   
+    res.render("users/login.ejs") // Access error message from flash
 }
 
 module.exports.signup =  async (req, res) => {
     try {
         let { username, email, password } = req.body; // Destructure the request body
-        const newUser = new User({ username, email }); // Create a new user object        const registeredUser = await User.register(newUser, password); // Register the user with the provided password
+        const newUser = new User({ username, email }); // Create a new user object
+        const registeredUser = await User.register(newUser, password); // Register the user with the provided password
         req.login(registeredUser, (err) => { // Log in the user
             if (err) {
                 console.error(err); // Log any error that occurs during login
@@ -19,9 +20,9 @@ module.exports.signup =  async (req, res) => {
             }
             req.flash("success", "Welcome to the site!"); // Set a success message in flash
             res.redirect("/listings"); // Redirect to the listings route
-        
+
         });
-       
+
     }
     catch (error) {
         console.error(error); // Log the error
